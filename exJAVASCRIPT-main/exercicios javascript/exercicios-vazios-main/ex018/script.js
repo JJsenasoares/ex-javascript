5c3f577ffc43c48f71de7856a28ba0f6e7e9af1e
Jhuan Pablo <j.pablo.sena@gmail.com>
2025-03-25 20:13:07 -0300

    Add files via upload

---
+++ exJAVASCRIPT-main/exercicios javascript/exercicios-vazios-main/ex018/script.js
@@ -0,0 +1,9 @@
+var salario = Number(prompt("digite seu salario"))
+var bonus = Number(prompt("digite o bonus: "))
+
+var salariototal = salario + bonus
+
+alert(`seu salario é ${salario} e voce recebeu im bonus de 
+    ${bonus}
+
+salario total = ${salariototal}`)
---
+++ exJAVASCRIPT-main/exercicios javascript/exercicios-vazios-main/ex019/script.js
@@ -0,0 +1,9 @@
+var altura = Number( prompt("Me de sua altura"))
+var base = Number( prompt("Me de sua base"))
+
+var resultado = area1 * area2 / 2 
+
+alert(`Base = ${base}
+    Altura = $(altura)
+    
+    A area do triamgulo = ${area}`)
